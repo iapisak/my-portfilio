60693d4a0ec600aa406e2090143039df1677d8b3
potisanmetee <iapisak1981@gmail.com>
2019-12-14 00:03:10 -0800

    initaial

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// javascript Animate onscroll Start
+    $(document).ready(function () {
+    if (screen.width > 1024) {
+    AOS.init({
+    easing: 'ease-in-out-sine',
+    once: true,
+    });
+    }
+    });
